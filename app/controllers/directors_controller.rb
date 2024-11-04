97365a77dba4d89976c707a2e5379c2b292c713f
ashtonbcheung <70730294+ashtonbcheung@users.noreply.github.com>
2024-11-04 06:32:00 +0000

    finished

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,10 +6,26 @@ class DirectorsController < ApplicationController
   def show
     the_id = params.fetch("the_id")
 
-    mathching_records = Director.where({ :id => the_id })
+    matching_records = Director.where({ :id => the_id })
 
-    @the_director = mathching_records.at(0)
+    @the_director = matching_records.at(0)
 
     render({ :template => "director_templates/details"})
+  end
+
+  def youngest
+    directors_desc_dob = Director.all.order({ :dob => :desc })
+
+    @youngest = directors_desc_dob.at(0)
+
+    render({ :template => "director_templates/youngest"})
+  end
+  
+  def eldest
+    directors_asc_dob = Director.all.order({ :dob => :asc })
+      
+    @eldest = directors_asc_dob.at(0)
+
+    render({ :template => "director_templates/eldest"})
   end
 end
